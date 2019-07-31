d1133cb34263d81314aeeedc12283095136aa0ef
angelD25 <atandel3@horizon.csueastbay.edu>
2019-07-31 11:47:27 -0700

    The main class of bank app is updated

--- BankApp/MainClass.java
+++ BankApp/MainClass.java
@@ -9,23 +9,50 @@ public class MainClass {
 		Scanner scan1 = new Scanner(System.in);
 		BankAcc user1 = new BankAcc();
 		
-		user1.accNumber = 1;
-		user1.balance = 1000;
-		user1.contactNum = 123;
-		user1.custName = "Anjali Tandel";
-		user1.email="anjalitandel56@gmail.com";
-		
+		user1.setAccNumber(1);
+		user1.setBalance(1000);
+		user1.setContactNum(123);
+		user1.setCustName("Anjali Tandel");
+		user1.setEmail("anjalitandel56@gmail.com");
 		int choice = begin();
-		
+		double withlimit = 100;
+		double depolimit = 100;
 		while(true) {
+		
 			if(choice!=1 && choice!=2 && choice!=3) {
 				return;
 			}
 			else if(choice == 1){
-				user1.deposit();
+				Scanner scan2 = new Scanner(System.in);
+				System.out.println("Enter the amount you want to deposit");
+				double amount = scan2.nextInt();
+				depolimit = depolimit - amount;
+				 if(depolimit < 0)
+				 {
+				 System.out.println("You can deposit upto 100 daily ");  
+				 }
+				 else
+				 {
+				 user1.deposit(amount);
+				 }	
 			}
 			else if(choice == 2) {
-				user1.withdraw();
+				System.out.println("You have " +withlimit+ " withdrawal limit");
+				 Scanner scan = new Scanner(System.in);
+				 System.out.println("Enter the amount you want to withdraw");
+				 double amount = scan.nextInt();
+				 if(amount > withlimit) {
+					 System.out.println("Daily limit for withdrawal is 100");
+				 }
+				 else {
+					 withlimit = withlimit-amount;
+					 if(withlimit < 0) { 
+					 System.out.println("Your limit for today is used");
+					 }
+					 else {
+						 user1.withdraw(amount);
+					 }
+				}
 			}
 			else if(choice == 3) {
 				System.out.println("Your balance is" + user1.getBalance());
