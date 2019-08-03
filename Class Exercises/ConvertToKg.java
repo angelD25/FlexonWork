d96453160f3990eaf5e794bfd8af1a8ef2d8f382
angelD25 <atandel3@horizon.csueastbay.edu>
2019-08-02 19:05:22 -0700

    Combined the Class exercises in One Folder

---
+++ Class Exercises/ConvertToKg.java
@@ -0,0 +1,15 @@
+package ExePackage;
+
+public class ConvertToKg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int apnd = 100;//pounds
+		double bkg;
+		double value = 0.45359237;
+		bkg = apnd*value;
+		System.out.println("The result is : " + bkg);
+		
+	}
+
+}
